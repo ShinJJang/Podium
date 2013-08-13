991304ac5c44c23d9f23de2d5b730d568f8b3fa6
Yeongmin Shin <makao.rule@gmail.com>
2013-08-13 17:01:56 +0900

    ./manage.py jenkins command enable

--- Podium/settings.py
+++ Podium/settings.py
@@ -152,3 +152,15 @@ LOGGING = {
         },
     }
 }
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',   # select one django or
+    #'django_jenkins.tasks.dir_tests'      # directory tests discovery
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount',
+    'django_jenkins.tasks.lettuce_tests',
+)
