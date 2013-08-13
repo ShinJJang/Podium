1c3c20c03d1ea534eb283b3fee85382437da8242
Yeongmin Shin <makao.rule@gmail.com>
2013-08-14 03:37:48 +0900

    parameter error in jenkins (TypeError: unbound method infer() must be called with Tuple instance as first argument (got InferenceContext instance instead))

--- Maple/views.py
+++ Maple/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', None)
