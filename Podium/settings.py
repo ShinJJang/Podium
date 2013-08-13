13a4cca04af7ad8d414a324b314f3a98981d4432
Yeongmin Shin <makao.rule@gmail.com>
2013-08-14 00:05:07 +0900

    db setttings modified

--- Podium/settings.py
+++ Podium/settings.py
@@ -14,9 +14,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'test',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
