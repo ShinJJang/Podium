cb489a3b82f2f601be5484fd563eb8508c20f068
Yeongmin Shin <makao.rule@gmail.com>
2013-08-14 01:45:49 +0900

    for test, model create

--- Maple/models.py
+++ Maple/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    field1 = models.IntegerField()
+    field2 = models.IntegerField()
+    field3 = models.IntegerField()
