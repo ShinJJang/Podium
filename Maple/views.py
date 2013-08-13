b4514fc9564198d963f388560c45f23e0d14d323
Yeongmin Shin <makao.rule@gmail.com>
2013-08-14 03:46:48 +0900

    method change(redner -> render_to_response)

--- Maple/views.py
+++ Maple/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
 from django.shortcuts import redirect
 
 def home(request):
-    return render(request, 'index.html', None)
+    return render_to_response('index.html')
