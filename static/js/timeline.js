075e36259a70e43457e48d948007ce4c3626aae5
Yeongmin Shin <makao.rule@gmail.com>
2013-08-27 06:57:45 +0900

    post polling

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -62,4 +62,22 @@ $('#form_post').submit(function(event) {
     });
     return false;
 });
+/* post polling */
+$(document).ready(function() {
+  (function poll() {
+    setTimeout(function() {
+        $.ajax({
+            url: "/api/v1/post/", // api inner parameter ?limit=20&offset=0"
+            type: "GET",
+            success: function(data) {
+                console.log("polling");
+                console.log(data)
+            },
+            dataType: "json",
+            complete: poll,
+            timeout: 5000
+        })
+    }, 5000);
+  })();
+});
 
