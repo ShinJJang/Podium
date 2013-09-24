762b44eadd8e755a8da46c8ff7145c77618b3b81
Yeongmin Shin <makao.rule@gmail.com>
2013-09-24 22:40:27 +0900

    js 정리 - comment polling 정지

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -1,74 +1,9 @@
-var post_top_url = "/api/v1/post/?limit=5&offset=40";
-var post_bottom_url = "/api/v1/post/";
+var post_top_url = "/api/v1/friendposts/?limit=5&offset=40";
+var post_bottom_url = "/api/v1/friendposts/";
 var comment_offsets = new Object();
 var isBottominit = 0;
 
-function writeComment() {
-    $('#form_comment').submit(function(event) {
-        var feedback_api = "/api/v1/comment/";
-        var data = JSON.stringify({
-            "comment": $("input[name=comment]").val(),
-            "post_key": $("input[name=post_key]").val()
-        });
-        console.log(data);
-        $.ajax({
-            url: feedback_api,
-            type: "POST",
-            contentType: "application/json",
-            data: data,
-            dataType: "json",
-            statusCode: {
-                201: function(data) {
-                    console.log("post submit response");
-                    console.log(data);
-                }
-            }
-        });
-        return false;
-    });
-};
-
-
-function focusComment(){
-    $(".p_commentList").waypoint(function(){
-        var tagid = $(this).attr("id");
-        console.log(tagid);
-        var postid = tagid.replace("commentList", "");
-        (function poll() {
-            console.log(postid);
-            setTimeout(function(){$.ajax({complete:poll()});pollComment(postid);}, 5000);
-        })();
-    }, { offset: "bottom-in-view"});
-}
-
-
-function pollComment(post_id) {
-    if(!post_id)
-        return;
-
-    get_uri = "/api/v1/comment/?post=" + post_id; // api inner parameter ?limit=20&offset=0"
-
-    if(comment_offsets[post_id]){
-        console.log("use offset in comment polling  " + comment_offsets[post_id]);
-        get_uri = "/api/v1/comment/?limit=5&offset=" + comment_offsets[post_id];
-    }
-    $.ajax({
-        url: get_uri,
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            console.log("polling comment");
-            console.log(data);
-            if(data.objects.length != 0)
-            {
-                $("#comment_template").tmpl(data.objects).appendTo("#commentList"+post_id);
-                comment_offsets[post_id] = data.meta.offset + data.objects.length;
-                console.log("오프셋"+comment_offsets[post_id]);
-                console.log("comment polling done "+comment_offsets[post_id]);
-            }
-        }
-        });
-}
+// post create
 $("#form_post").submit(function(event) {
     alert("@");
     var feedback_api = "/api/v1/post/";
@@ -87,55 +22,73 @@ $("#form_post").submit(function(event) {
                 /*post로 생성 후 생성한 json response*/
                 console.log("post submit response");
                 console.log(data);
-//                $('#post_public_template').tmpl(data).prependTo('#timeline');
             }
         }
     });
     return false;
 });
 
-function timelineRefresh() {
+// comment create
+$(document).on("submit", "#form_comment",function(event) {
+        var feedback_api = "/api/v1/comment/";
+        var data = JSON.stringify({
+            "comment": $("input[name=comment]").val(),
+            "post_key": $("input[name=post_key]").val()
+        });
+        console.log(data);
+        $.ajax({
+            url: feedback_api,
+            type: "POST",
+            contentType: "application/json",
+            data: data,
+            dataType: "json",
+            statusCode: {
+                201: function(data) {
+                    console.log("post submit response");
+                    console.log(data);
+                    pollComment($("input[name=post_key]").val());
+                }
+            }
+        });
+        return false;
+});
+
+// polling post
+function PostTopPolling() {
     $.ajax({
         url: post_top_url,
         type: "GET",
         dataType: "json",
         success: function(data) {
-            console.log("TTTTTTTTTTTT polling post" + post_top_url);
+            console.log("TOP POLL POST  url:" + post_top_url);
             console.log(data)
             if(data.objects.length != 0)
             {
                 $("#post_public_template").tmpl(data.objects.reverse()).prependTo("#timeline");
                 $.waypoints('refresh');
-                writeComment();
-                $(".p_responses").click(function(){
-                    $(this).parent().children("section").toggle();
-                    $(this).toggleClass("opened");
-                    return false;
-                });
                 post_top_url = data.meta.next;
                 if(!post_top_url)
                     post_top_url = "api/v1/post/?limit=20&offset=" + (data.meta.offset+data.objects.length);
                 if(isBottominit==0) {
                     post_bottom_url = data.meta.previous;
-                    console.log(post_bottom_url);
+                    console.log("previous url init "+post_bottom_url);
                     isBottominit = 1;
                 }
-                focusComment();
+//                focusComment();
             }
         }
         });
 }
 
-/* post polling */
+// when screen on top, call PostTopPolling
 $(document).ready(function() {
-  timelineRefresh();
+  PostTopPolling();
+  $("#p_timeline").waypoint(function(){postBottom();}, { offset: 'bottom-in-view' });
+  $("#p_timeline").waypoint(function(){
   (function poll() {
-    setTimeout(function(){$.ajax({complete:poll()});timelineRefresh();}, 5000);
-  })();
-  $("#p_timeline").waypoint(function(){console.log("start bottom   "+post_bottom_url);postBottom();}, { offset: 'bottom-in-view' });
-
+    setTimeout(function(){PostTopPolling();$.ajax({complete:poll()});}, 5000);
+  })()}, { offset: '0' })
 });
-
 
 function postBottom() {
     if(!post_bottom_url)
@@ -148,16 +101,68 @@ function postBottom() {
             if(data.objects.length != 0)
             {
                 $("#post_public_template").tmpl(data.objects.reverse()).appendTo("#timeline");
-                $(".p_responses").click(function(){
-                    $(this).parent().children("section").toggle();
-                    $(this).toggleClass("opened");
-                    return false;
-                });
-                writeComment();
                 post_bottom_url = data.meta.previous;
-                console.log("BBBBBBBBBB polling post" + post_bottom_url);
+                console.log("BOTTOM POLL POST   url:" + post_bottom_url);
             }
         }
         });
 }
 
+// when on post, call pollComment
+function focusComment(){
+    $(".p_commentList").waypoint(function(){
+        var tagid = $(this).attr("id");
+        if (!tagid)
+            return false;
+        var postid = tagid.replace("commentList", "");
+        pollComment(postid);
+        (function poll() {
+            console.log(postid);
+            setTimeout(function(){$.ajax({complete:poll()});pollComment(postid);}, 60000);
+        })();
+    }, { offset: "bottom-in-view"});
+}
+
+$(document).on("waypoint", { offset: "bottom-in-view"}, ".p_commentList", function(){
+    var tagid = $(this).attr("id");
+        if (!tagid)
+            return false;
+        var postid = tagid.replace("commentList", "");
+        pollComment(postid);
+        (function poll() {
+            console.log(postid);
+            setTimeout(function(){$.ajax({complete:poll()});pollComment(postid);}, 60000);
+        })();
+})
+
+// polling comment
+function pollComment(post_id) {
+    if(!post_id)
+        return;
+
+    get_uri = "/api/v1/comment/?post=" + post_id; // api inner parameter ?limit=20&offset=0"
+
+    if(comment_offsets[post_id]){
+        get_uri = "/api/v1/comment/?limit=30&offset=" + comment_offsets[post_id];
+    }
+    $.ajax({
+        url: get_uri,
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            if(data.objects.length != 0)
+            {
+                $("#comment_template").tmpl(data.objects).appendTo("#commentList"+post_id);
+                comment_offsets[post_id] = data.meta.offset + data.objects.length;
+                console.log("댓글 폴링한 마지막 오프셋 :"+comment_offsets[post_id]);
+            }
+        }
+        });
+}
+
+// comment toggle
+$(document).on("click", ".p_responses", function(){
+                    $(this).parent().children("section").toggle();
+                    $(this).toggleClass("opened");
+                    return false;
+                });
