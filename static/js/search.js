d99c681fa3066b8bc509fbf4e053fca6d995c97f
Yeongmin Shin <makao.rule@gmail.com>
2013-11-03 23:57:34 +0900

    상단 검색 구현 - 현재 유저명, 그룹명 지원 - select2 안쓰고 처리함

--- static/js/search.js
+++ static/js/search.js
@@ -1,17 +1,28 @@
 $('#p_topSearchForm').keyup(function() {
     if(!$(this).val())
         return false;
-    var feedback_api = '/api/v1/user/search/?q='+$(this).val();
+    var user_feedback_api = '/api/v1/user/search/?q='+$(this).val();
+    var group_feedback_api = '/api/v1/groups/search/?q='+$(this).val();
+    $("#p_topSearch_results").html('');
     $.ajax({
-        url: feedback_api,
+        url: user_feedback_api,
         type: "GET",
         dataType: "json",
         success: function(data) {
-            if(data.objects.length == 0) {
-                 $('#p_topSearch_results').html('');
+            for(var index in data.objects){
+                console.log(data);
+                $('#p_topSearch_results').append('<li><a href=/people/'+data.objects[index].id+'>'+data.objects[index].username+'</a></li>');
             }
-            for(index in data.objects){
-                $('#p_topSearch_results').html('<li>'+data.objects[index].username+'</li>');
+        }
+    });
+    $.ajax({
+        url: group_feedback_api,
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            for(var index in data.objects){
+                console.log(data);
+                $('#p_topSearch_results').append('<li><a href=/group/'+data.objects[index].id+'>'+data.objects[index].group_name+'</a></li>');
             }
         }
     });
